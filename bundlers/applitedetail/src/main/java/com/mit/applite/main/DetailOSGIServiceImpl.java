e7badbc2f2f8185d11d9b8fff828d3d981dd6bc1
huangxiaodong <huangxiaodong@leadexceed.com>
2015-06-29 14:52:20 +0800

    [summary][merge 5926b024][] [bug-id/modified mod][] [c marco/ java key value][] [tester-suggest][none]

--- bundlers/applitedetail/src/main/java/com/mit/applite/main/DetailOSGIServiceImpl.java
+++ bundlers/applitedetail/src/main/java/com/mit/applite/main/DetailOSGIServiceImpl.java
@@ -26,8 +26,8 @@ public class DetailOSGIServiceImpl implements ApkplugOSGIService {
             bundle.putString("name", (String) objs[2]);
             bundle.putString("imgUrl", (String) objs[3]);
             fg.setArguments(bundle);
-            ft.hide(fgm.findFragmentByTag(Constant.OSGI_SERVICE_SEARCH_FRAGMENT));//得到搜索Fragment，然后隐藏
-            ft.add(node, fg, Constant.OSGI_SERVICE_DETAIL_FRAGMENT);
+//            ft.hide(fgm.findFragmentByTag(Constant.OSGI_SERVICE_SEARCH_FRAGMENT));//得到搜索Fragment，然后隐藏
+            ft.replace(node, fg, Constant.OSGI_SERVICE_DETAIL_FRAGMENT);
             ft.addToBackStack(null);
             ft.commit();
         }
--- bundlers/appliteupdate/src/main/java/com/mit/appliteupdate/main/UpdateOSGIServiceImpl.java
+++ bundlers/appliteupdate/src/main/java/com/mit/appliteupdate/main/UpdateOSGIServiceImpl.java
@@ -21,8 +21,8 @@ public class UpdateOSGIServiceImpl implements ApkplugOSGIService {
             Fragment fg = new UpdateFragment();
             FragmentManager fgm = (FragmentManager) objs[0];
             FragmentTransaction ft = fgm.beginTransaction();
-            ft.hide(fgm.findFragmentByTag(Constant.OSGI_SERVICE_MAIN_FRAGMENT));//得到首页Fragment，然后隐藏
-            ft.add(node, fg, Constant.OSGI_SERVICE_UPDATE_FRAGMENT);
+//            ft.hide(fgm.findFragmentByTag(Constant.OSGI_SERVICE_MAIN_FRAGMENT));//得到首页Fragment，然后隐藏
+            ft.replace(node, fg, Constant.OSGI_SERVICE_UPDATE_FRAGMENT);
             ft.addToBackStack(null);
             ft.commit();
         }
