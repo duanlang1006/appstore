0975c7c39fef0dd64d15d06731e699cedab21de2
DuanLang <duanlang@leadexceed.com>
2015-08-19 17:52:55 +0800

    [summary][][首页进入二级菜单时back键退出逻辑错误问题] [bug-id/modified mod][] [c marco/ java key value][] [tester-suggest][none]

--- bundlers/applitehomepage/src/main/java/com/applite/homepage/HomePageFragment.java
+++ bundlers/applitehomepage/src/main/java/com/applite/homepage/HomePageFragment.java
@@ -14,6 +14,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -231,6 +232,22 @@ public class HomePageFragment extends OSGIBaseFragment implements View.OnClickLi
     public void onResume() {
         super.onResume();
         MobclickAgent.onPageStart(whichPage); //统计页面
+
+        getView().setFocusableInTouchMode(true);
+        getView().requestFocus();
+        getView().setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_BACK) {
+                    // handle back button
+                    if (!getFragmentManager().popBackStackImmediate()) {
+                        mActivity.finish();
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
